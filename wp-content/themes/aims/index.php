4a94843d0477aedb1c6d8cf59e8940424ad16996
hassan <hassantahir2050@gmail.com>
2014-09-29 10:59:15 +0500

    changing the styling in home page

--- wp-content/themes/aims/index.php
+++ wp-content/themes/aims/index.php
@@ -4,8 +4,8 @@
     
     <?php //echo theme_home_box_container("",theme_home_box());?>
     <?php echo theme_slider();?>
-    <?php echo theme_slider_content();?>
-    <?php echo do_shortcode('[wonderplugin_carousel id="1"]'); ?>
+    <?php //echo theme_slider_content();?>
+    <?php echo do_shortcode(theme_slider_content().'[wonderplugin_carousel id="1"]'); ?>
 
 
     
