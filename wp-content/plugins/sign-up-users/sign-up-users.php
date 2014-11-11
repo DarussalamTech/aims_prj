5753cef834e6e93a7d3ac0572bef4aae9b87e76b
Niki John <naeem.mushtaq@dtechsystems.net>
2014-11-11 16:28:07 +0500

    adding login link

--- wp-content/plugins/sign-up-users/sign-up-users.php
+++ wp-content/plugins/sign-up-users/sign-up-users.php
@@ -24,7 +24,7 @@ function signupuser() {
 
         <div  class="quest_name" style="text-align: center">
     <? echo 'Welcome, ' . $current_user->user_login; ?> </div>
- <div  class="quest_name" style="text-align: center"> Click Here To View Avalaible Courses <a title="Login" href="<?php echo WP_SITEURL . '/student-available-courses'; ?>">Available Courses</a>
+ <div  class="quest_name" style="text-align: center"> Click Here To View Avalaible Courses <a title="Available Courses" href="<?php echo WP_SITEURL . '/student-available-courses'; ?>">Available Courses</a>
 
     </div>
 
@@ -97,7 +97,7 @@ function signupuser() {
                 }
 ?>
                 <div  class="quest_name" style="text-align: center">
-                    Registered successfully Please <a title="Login" href="<?php echo WP_SITEURL . '/wp-login.php'; ?>">Login</a>
+                    Registered successfully.
                 </div>
 <?
             }
@@ -128,6 +128,9 @@ function signupuser() {
         <div><input class="button-secondary" type="submit"  value="sign-up"></div>
     </form>
 </div>
+    <div  class="quest_name" style="text-align: center">
+        <a title="Login" href="<?php echo WP_SITEURL . '/wp-login.php'; ?>">Login</a>
+    </div>
 <?php
         }
     }
